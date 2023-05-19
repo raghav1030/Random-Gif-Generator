81a54cdcdf13371455181ab1ba6d118e09a68289
raghav1030 <raghav123ggandhi@gmail.com>
2023-05-19 16:16:39 +0530

    Added placeholder at tag

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -16,7 +16,7 @@ export default function Tag() {
 
           {!loading ? <img src={gif} alt="" width='450' /> : <Spinner/>}
 
-          <input type="text" value={tag} onChange={(event)=>{setTag(event.target.value)}} className='mb-[15px] w-10/12 text-lg py-2 px-5 rounded-lg' />
+          <input type="text" value={tag} onChange={(event)=>{setTag(event.target.value)}} placeholder='Enter a prompt for random gif generation' className='mb-[15px] w-10/12 text-lg py-2 px-5 rounded-lg' />
 
           <button className='mb-[15px] w-10/12 bg-orange-300 text-lg py-2 rounded-lg hover:bg-orange-400 transition-all duration-150 ' onClick={()=>{
             fetchData(tag)
